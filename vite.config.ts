560a79b71921575847285b8a13cf24a89c460414
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-04-18 17:36:33 +0700

    build: update pwa option

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +10,13 @@ export default defineConfig({
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "Counter",
+        name: "BomSLap - Counter",
+        theme_color: "#222831",
+      },
+    }),
   ],
 });
